7706b0e3daaa28ddba27f8db765206891719976b
Soaib Ahmed <129555727+SOAIB56@users.noreply.github.com>
2024-12-29 13:34:07 +0600

    Add files via upload

---
+++ django_1/django_model/project_4/myApp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+# from . import djangoForm
+
+
+
+urlpatterns = [
+    path("",views.home, name = "homepage"),
+    path("about/", views.about, name = "about"),
+    path("form/", views.form, name = "form"),
+    path("djangoForm/", views.DjangoForm, name = "djangoForm"),
+    path("studentForm/", views.StudentFormData, name = "studentForm"),
+]
---
+++ django_1/django_model/project_4/myApp/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "home.html")
+
+def about(request):
+     if request.method == "POST":
+        name = request.POST.get("userName")
+        email = request.POST.get("email")
+        return render(request, "about.html", {"name":name, "email": email})
+     elif request.method == "GET":
+        name = request.GET.get("userName")
+        email = request.GET.get("email")
+        return render(request, "about.html", {"name":name, "email": email})
+
+     else:
+         return render(request, "about.html")
+
+def form(request):
+    # print(request.POST)
+   return render(request, "form.html")
+
+
+from . forms import Contact
+from . forms import StudentData
+
+
+
+def DjangoForm(request):
+    # form = Contact(request.POST)
+    # if form.is_valid():
+    #     print(form.cleaned_data) 
+    # return render(request, "django_form.html", {"form": form})
+
+    if request.method == "POST":
+        form = Contact(request.POST, request.FILES)
+        if form.is_valid():
+            file = form.cleaned_data["profile_pic"] # profile_pic is file name of forms.py file            
+            with open("./myApp/upload/" + file.name, "wb+") as destination:
+                for chunk in file.chunks():
+                    destination.write(chunk)
+
+            print(form.cleaned_data) 
+            return render(request, "django_form.html", {"form": form})
+    else:
+        form = Contact()
+    
+    return render(request, "django_form.html", {"form": form})
+
+
+
+def  StudentFormData(request):
+    if request.method == "POST":
+        form = StudentData(request.POST, request.FILES)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = StudentData()
+    return render(request, "django_form.html", {"form": form}) 
